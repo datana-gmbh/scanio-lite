5685f491ffcb2edbf862bfe1544565ec5195c9a2
Oskar Stark <oskarstark@googlemail.com>
2023-12-06 12:52:11 +0100

    -

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route(name: Routes::DASHBOARD, path: '/dashboard')]
+#[Route(name: Routes::DASHBOARD, path: '/')]
 final readonly class DashboardController
 {
     public function __construct(
