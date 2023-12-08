7629234c00763e3c522171318774088965c638ca
Oskar Stark <oskarstark@googlemail.com>
2023-12-08 12:13:51 +0100

    -

--- src/Fixtures/UserFixtures.php
+++ src/Fixtures/UserFixtures.php
@@ -5,19 +5,20 @@ declare(strict_types=1);
 namespace App\Fixtures;
 
 use App\Fixtures\Factory\DocumentFactory;
+use App\Fixtures\Factory\UserFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 
-final class DocumentFixtures extends Fixture implements OrderedFixtureInterface
+final class UserFixtures extends Fixture implements OrderedFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
-        DocumentFactory::createMany(10);
+        UserFactory::createMany(8);
     }
 
     public function getOrder(): int
     {
-        return 3;
+        return 1;
     }
 }
