29ae70f3fc50a11d124a1268501197abb01587fd
Silas Joisten <silasjoisten@proton.me>
2023-12-16 10:32:01 +0100

    Enhancement: Adds E-Mail entity (#100)

---
+++ src/Fixtures/Factory/EmailFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Fixtures\Factory;
+
+use App\Entity\Email;
+use Zenstruck\Foundry\ModelFactory;
+
+/**
+ * @extends ModelFactory<Email>
+ */
+final class EmailFactory extends ModelFactory
+{
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories
+     */
+    protected function getDefaults(): array
+    {
+        $faker = self::faker();
+
+        return [
+            'sender' => $faker->email(),
+            'receiver' => $faker->companyEmail(),
+            'subject' => $faker->sentence(),
+            'body' => $faker->realText($faker->numberBetween(100, 500)),
+        ];
+    }
+
+    protected static function getClass(): string
+    {
+        return Email::class;
+    }
+}
