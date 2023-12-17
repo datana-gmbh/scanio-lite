5a9c7b37edfa18186a3b7612636cdec5d5b7b9ff
Oskar Stark <oskarstark@googlemail.com>
2023-12-17 11:47:38 +0100

    Fix used repository

--- src/Entity/Email.php
+++ src/Entity/Email.php
@@ -6,14 +6,14 @@ namespace App\Entity;
 
 use App\Bridge\Doctrine\DBAL\Types\Type\Identifier\EmailIdType;
 use App\Domain\Identifier\EmailId;
-use App\Repository\SourceRepository;
+use App\Repository\EmailRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\ORM\Mapping\Column;
 use Doctrine\ORM\Mapping\Id;
 use Safe\DateTimeImmutable;
 
-#[ORM\Entity(repositoryClass: SourceRepository::class)]
+#[ORM\Entity(repositoryClass: EmailRepository::class)]
 #[ORM\Table(name: 'emails')]
 class Email
 {
